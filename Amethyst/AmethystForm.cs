4b52d287ed097cfd5bd8f1cf81b796c329cbca11
izrik <izrik@aa2253e4-891b-cf47-b6be-b091cf79ccf8>
2010-05-19 20:51:24 +0000

    git-svn-id: file:///srv/svn/metaindu@222 aa2253e4-891b-cf47-b6be-b091cf79ccf8

---
+++ Amethyst/AmethystForm.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using MetaphysicsIndustries.Crystalline;
+using MetaphysicsIndustries.Amethyst;
+
+namespace Amethyst
+{
+    public partial class AmethystForm : CrystallineAppForm<AmethystControl>
+    {
+        public AmethystForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override string DefaultExtension
+        {
+            get { return "amethyst"; }
+        }
+
+        protected override string Filter
+        {
+            get { return "Amethyst files (*.amethyst)|*.amethyst|All files (*.*)|*.*"; }
+        }
+
+        protected override string AppTitle
+        {
+            get { return "Amethyst"; }
+        }
+    }
+}
+
---
+++ Amethyst/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Amethyst")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Metaphysics Industries, Inc.")]
+[assembly: AssemblyProduct("Amethyst")]
+[assembly: AssemblyCopyright("Copyright © Metaphysics Industries, Inc. 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f04332e7-6ebf-4b8b-9abd-086c620294e0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
